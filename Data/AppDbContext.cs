52bcb2ce44687f64e3fa1ae6b2e1124ae30567d3
Ray <118784122+SayuriObana@users.noreply.github.com>
2025-04-03 15:18:41 -0300

    Atualiza .gitignore e limpa arquivos desnecessários

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotasApi.Models;
+
+namespace NotasApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Nota> Notas => Set<Nota>();
+    }
+}
